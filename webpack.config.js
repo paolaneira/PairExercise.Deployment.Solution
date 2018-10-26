f47d0028047530dd7ae83e1117d174a8cf93f4bf
Paola Neira <paola1792@gmail.com>
2018-10-26 09:23:39 -0400

    Test Travis

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: [
-    '@babel/polyfill', // enables async-await
-    './client/index.js'
+    "@babel/polyfill", // enables async-await
+    "./client/index.js"
   ],
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: "./public/bundle.js"
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       }
     ]
   }
-}
+};
